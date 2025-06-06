e8cbfd360ad3c9d2abe21409ce403d1e81b155de
littwa <littwa777@gmail.com>
2025-06-06 10:59:29 +0300

    fix dep 24

--- src/modules/screenshot/screenshot.module.ts
+++ src/modules/screenshot/screenshot.module.ts
@@ -1,18 +1,18 @@
-import { Module } from '@nestjs/common';
-import { ScreenshotService } from './screenshot.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Screenshot, ScreenshotSchema } from 'src/modules/screenshot/screenshot.schema';
-import { ScreenshotList, ScreenshotListSchema } from 'src/modules/screenshot/screenshot-list.schema';
-import { CommonService } from 'src/shared/services/common.service';
-import { ScreenshotController } from 'src/modules/screenshot/screenshot.controller';
-
-@Module({
-  imports: [MongooseModule.forFeature([
-    { name: Screenshot.name, schema: ScreenshotSchema },
-    { name: ScreenshotList.name, schema: ScreenshotListSchema }
-  ])],
-  controllers: [ScreenshotController],
-  providers: [ScreenshotService, CommonService],
-  exports: [ScreenshotService]
-})
-export class ScreenshotModule {}
+// import { Module } from '@nestjs/common';
+// import { ScreenshotService } from './screenshot.service';
+// import { MongooseModule } from '@nestjs/mongoose';
+// import { Screenshot, ScreenshotSchema } from 'src/modules/screenshot/screenshot.schema';
+// import { ScreenshotList, ScreenshotListSchema } from 'src/modules/screenshot/screenshot-list.schema';
+// import { CommonService } from 'src/shared/services/common.service';
+// import { ScreenshotController } from 'src/modules/screenshot/screenshot.controller';
+//
+// @Module({
+//   imports: [MongooseModule.forFeature([
+//     { name: Screenshot.name, schema: ScreenshotSchema },
+//     { name: ScreenshotList.name, schema: ScreenshotListSchema }
+//   ])],
+//   controllers: [ScreenshotController],
+//   providers: [ScreenshotService, CommonService],
+//   exports: [ScreenshotService]
+// })
+// export class ScreenshotModule {}
