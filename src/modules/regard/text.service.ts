7237ea30c43e22f9b8d880748be415b04615ee0c
littwa <littwa777@gmail.com>
2025-06-06 14:18:49 +0300

    fix dep 47

--- src/modules/regard/text.service.ts
+++ src/modules/regard/text.service.ts
@@ -1,19 +1,19 @@
-// import { Injectable } from '@nestjs/common';
-// import { InjectModel } from '@nestjs/mongoose';
-// import { Regard, RegardDocument } from './regard.schema';
-// import { Model } from 'mongoose';
-// import { Text, TextDocument } from './text.schema';
-// import { CommonService } from '../../shared/services/common.service';
-//
-// @Injectable()
-// export class TextService {
-//   constructor(
-//     // @InjectModel(Regard.name) public regardModel: Model<RegardDocument>,
-//     @InjectModel(Text.name) private textModel: Model<TextDocument>,
-//   ) {
-//   }
-//
-//   async find(body) {
-//      return this.textModel.findOne({ content: body.content });
-//   }
-// }
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Regard, RegardDocument } from './regard.schema';
+import { Model } from 'mongoose';
+import { Text, TextDocument } from './text.schema';
+import { CommonService } from '../../shared/services/common.service';
+
+@Injectable()
+export class TextService {
+  constructor(
+    // @InjectModel(Regard.name) public regardModel: Model<RegardDocument>,
+    @InjectModel(Text.name) private textModel: Model<TextDocument>,
+  ) {
+  }
+
+  async find(body) {
+     return this.textModel.findOne({ content: body.content });
+  }
+}
